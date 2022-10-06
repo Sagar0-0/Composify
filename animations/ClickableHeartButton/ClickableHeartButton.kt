ca19caa51ef13781df4572171c0ec0e5c6dd62a2
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-10-06 23:16:01 +0530

    Create ClickableHeartButton.kt

---
+++ animations/ClickableHeartButton/ClickableHeartButton.kt
@@ -0,0 +1,69 @@
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+//change these values according to your preference
+private val idleIconSize = 50.dp
+private val expandedIconSize = 60.dp
+
+@Composable
+@Preview
+fun ClickableHeartButton() {
+    var needLike by remember { mutableStateOf(true) }
+    val transition = updateTransition(targetState = needLike, label = "LikeTransition")
+
+    val animatedSizeDp by transition.animateDp(label = "sizeTransition",
+        transitionSpec = {
+            keyframes {
+                //change these values according to your preference
+                durationMillis = 150
+                expandedIconSize at 80
+                idleIconSize at 140
+            }
+        }) { needLike ->
+        //in both like and not like cases, heart will have same size, we just need the animation here.
+        if (needLike) {
+            idleIconSize
+        } else {
+            idleIconSize
+        }
+    }
+
+    val heartColor by transition.animateColor(
+        transitionSpec = { tween(100) }, label = "colorTransition"
+    ) { needLike ->
+        if (needLike) {//if heart is not liked, then it will have..
+            Color.Gray
+        } else {//if heart is already liked, then it will have..
+            Color.Red
+        }
+    }
+    Image(
+        modifier = Modifier
+            .size(animatedSizeDp)
+            .clickable(
+                interactionSource = remember {
+                    MutableInteractionSource()
+                },
+                indication = null,
+                onClick = { needLike = !needLike }
+            ),
+        imageVector = Icons.Default.Favorite,
+        contentDescription = "",
+        colorFilter = ColorFilter.tint(heartColor)
+    )
+}
