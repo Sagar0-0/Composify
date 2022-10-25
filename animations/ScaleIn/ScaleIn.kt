97bfcd70a86ad5ddb40562461ed790ca05e81a00
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2022-10-25 21:51:40 +0530

    Update ScaleIn.kt

--- animations/ScaleIn/ScaleIn.kt
+++ animations/ScaleIn/ScaleIn.kt
@@ -16,31 +16,33 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.TransformOrigin
 
-Column {
-    var showRed by remember { mutableStateOf(true) }
-    var showGreen by remember { mutableStateOf(true) }
+fun ScaleIn() {
+    Column {
+        var showRed by remember { mutableStateOf(true) }
+        var showGreen by remember { mutableStateOf(true) }
 
-    AnimatedVisibility(
-        visible = showGreen,
-        enter = scaleIn() + expandVertically(expandFrom = Alignment.CenterVertically),
-        exit = scaleOut() + shrinkVertically(shrinkTowards = Alignment.CenterVertically)
-    ) {
-        Box(
-            Modifier.size(100.dp)
-                .background(color = Color.Green, shape = RoundedCornerShape(20.dp))
-        )
-    }
+        AnimatedVisibility(
+            visible = showGreen,
+            enter = scaleIn() + expandVertically(expandFrom = Alignment.CenterVertically),
+            exit = scaleOut() + shrinkVertically(shrinkTowards = Alignment.CenterVertically)
+        ) {
+            Box(
+                Modifier.size(100.dp)
+                    .background(color = Color.Green, shape = RoundedCornerShape(20.dp))
+            )
+        }
 
-    AnimatedVisibility(
-        visible = showRed,
-        enter = scaleIn(transformOrigin = TransformOrigin(0f, 0f)) +
-            fadeIn() + expandIn(expandFrom = Alignment.TopStart),
-        exit = scaleOut(transformOrigin = TransformOrigin(0f, 0f)) +
-            fadeOut() + shrinkOut(shrinkTowards = Alignment.TopStart)
-    ) {
-        Box(
-            Modifier.size(100.dp)
-                .background(color = Color.Red, shape = RoundedCornerShape(20.dp))
-        )
+        AnimatedVisibility(
+            visible = showRed,
+            enter = scaleIn(transformOrigin = TransformOrigin(0f, 0f)) +
+                fadeIn() + expandIn(expandFrom = Alignment.TopStart),
+            exit = scaleOut(transformOrigin = TransformOrigin(0f, 0f)) +
+                fadeOut() + shrinkOut(shrinkTowards = Alignment.TopStart)
+        ) {
+            Box(
+                Modifier.size(100.dp)
+                    .background(color = Color.Red, shape = RoundedCornerShape(20.dp))
+            )
+        }
     }
 }
